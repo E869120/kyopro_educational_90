ff50704741d05212feb6c161a14c1670d7ad8dc0
Masataka Yoneda <e869120@gmail.com>
2021-03-31 07:26:00 +0900

    Modified Editorial of 001

--- sol/001.cpp
+++ sol/001.cpp
@@ -17,14 +17,14 @@ bool solve(long long M) {
 }
 
 int main() {
-	// Step #1. 入力
-	cin >> N >> L;
+    // Step #1. 入力
+    cin >> N >> L;
     cin >> K;
-	for (int i = 1; i <= N; i++) {
-		cin >> A[i];
-	}
-
-	// Step #2. 答えで二分探索（めぐる式二分探索法）
+    for (int i = 1; i <= N; i++) {
+        cin >> A[i];
+    }
+    
+    // Step #2. 答えで二分探索（めぐる式二分探索法）
     // https://qiita.com/drken/items/97e37dd6143e33a64c8c
     long long left = -1;
     long long right = L + 1;
@@ -34,5 +34,5 @@ int main() {
         else left = mid;
     }
     cout << left << endl;
-	return 0;
+    return 0;
 }

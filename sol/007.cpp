11af93997a8b7bc3550b0177e0ca1b439e0f8440
Masataka Yoneda <e869120@gmail.com>
2021-04-07 07:39:03 +0900

    Add Problem 007

---
+++ sol/007.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+const int INF = 2000000000;
+int N;
+int A[100009];
+int Q;
+int B[100009];
+
+int main() {
+	// Step #1. 入力
+	cin >> N;
+	for (int i = 1; i <= N; i++) cin >> A[i];
+	cin >> Q;
+	for (int i = 1; i <= Q; i++) cin >> B[i];
+
+	// Step #2. ソート
+	sort(A + 1, A + N + 1);
+
+	// Step #3. クエリ処理
+	for (int i = 1; i <= Q; i++) {
+		int pos1 = lower_bound(A + 1, A + N + 1, B[i]) - A;
+		int Diff1 = INF, Diff2 = INF;
+		if (pos1 <= N) Diff1 = abs(B[i] - A[pos1]);
+		if (pos1 >= 2) Diff2 = abs(B[i] - A[pos1 - 1]);
+		cout << min(Diff1, Diff2) << endl;
+	}
+	return 0;
+}

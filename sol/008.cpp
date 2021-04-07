0db9db54b7b8b2dca43f60f502dbf5f2f510878e
Masataka Yoneda <e869120@gmail.com>
2021-04-08 07:44:11 +0900

    Add Problem 008

---
+++ sol/008.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+string S;
+long long mod = 1000000007;
+long long dp[100009][8];
+
+int main() {
+	// Step #1. “ü—Í
+	cin >> S;
+
+	// Step #2. ó‘Ô DP
+	dp[0][0] = 1;
+	for (int i = 0; i < (int)S.size(); i++) {
+		for (int j = 0; j <= 7; j++) {
+			dp[i + 1][j] += dp[i][j];
+			if (S[i] == 'a' && j == 0) dp[i + 1][j + 1] += dp[i][j];
+			if (S[i] == 't' && j == 1) dp[i + 1][j + 1] += dp[i][j];
+			if (S[i] == 'c' && j == 2) dp[i + 1][j + 1] += dp[i][j];
+			if (S[i] == 'o' && j == 3) dp[i + 1][j + 1] += dp[i][j];
+			if (S[i] == 'd' && j == 4) dp[i + 1][j + 1] += dp[i][j];
+			if (S[i] == 'e' && j == 5) dp[i + 1][j + 1] += dp[i][j];
+			if (S[i] == 'r' && j == 6) dp[i + 1][j + 1] += dp[i][j];
+		}
+		for (int j = 0; j <= 7; j++) dp[i + 1][j] %= mod;
+	}
+
+	// Step #3. o—Í
+	cout << dp[S.size()][7] << endl;
+	return 0;
+}

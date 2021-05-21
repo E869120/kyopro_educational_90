f1e59bb83cea47c59d74af6fbf515c7f1949981e
Masataka Yoneda <e869120@gmail.com>
2021-05-22 07:50:42 +0900

    Add Problem 046

---
+++ sol/046.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+int main() {
+	int N;
+	cin >> N;
+	vector<int> A(N), B(N), C(N);
+	for (int i = 0; i < N; ++i) cin >> A[i];
+	for (int i = 0; i < N; ++i) cin >> B[i];
+	for (int i = 0; i < N; ++i) cin >> C[i];
+	vector<int> am(46), bm(46), cm(46);
+	for (int i = 0; i < N; ++i) {
+		++am[A[i] % 46];
+		++bm[B[i] % 46];
+		++cm[C[i] % 46];
+	}
+	long long ans = 0;
+	for (int i = 0; i < 46; ++i) {
+		for (int j = 0; j < 46; ++j) {
+			for (int k = 0; k < 46; ++k) {
+				if ((i + j + k) % 46 == 0) {
+					ans += (long long)(am[i]) * bm[j] * cm[k];
+				}
+			}
+		}
+	}
+	cout << ans % 1000000007 << endl;
+	return 0;
+}

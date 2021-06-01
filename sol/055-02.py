64eb1068ac66b4d7826adc354a59ef1bae6dfe2b
Masataka Yoneda <e869120@gmail.com>
2021-06-02 07:58:32 +0900

    Add Problem 055

--- sol/055-02.py
+++ sol/055-02.py
@@ -7,7 +7,7 @@ for i in range(0, N):
         for k in range(j + 1, N):
             for l in range(k + 1, N):
                 for m in range(l + 1, N):
-                    v = (A[i] * A[j] * A[k] * A[l] * A[m]) % P
+                    v = ((((A[i] * A[j] % P) * A[k] % P) * A[l] % P) * A[m]) % P
                     if v == Q:
                         Answer += 1
  
